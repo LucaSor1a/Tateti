103765575d42c90a885daa82dc2ae6a44d15bbf6
Lucas Soria <l.soria@alumno.um.edu.ar>
2019-10-30 11:25:56 -0300

    Mejora

--- tateti.py
+++ tateti.py
@@ -39,48 +39,28 @@ class TaTeTi():
         return Draw
 
     def check_row(self):
-        if self.board[0] == 'x' and self.board[1] == 'x' and self.board[2] == 'x':
-            return True
-        if self.board[3] == 'x' and self.board[4] == 'x' and self.board[5] == 'x':
-            return True
-        if self.board[6] == 'x' and self.board[7] == 'x' and self.board[8] == 'x':
-            return True
-        if self.board[0] == 'o' and self.board[1] == 'o' and self.board[2] == 'o':
-            return True
-        if self.board[3] == 'o' and self.board[4] == 'o' and self.board[5] == 'o':
-            return True
-        if self.board[6] == 'o' and self.board[7] == 'o' and self.board[8] == 'o':
-            return True
-        else:
-            return False
+        for i in range(3):
+            if self.board[(i*3)+0] == 'x' and self.board[(i*3)+1] == 'x' and self.board[(i*3)+2] == 'x':
+                return True
+            if self.board[(i*3)+0] == 'o' and self.board[(i*3)+1] == 'o' and self.board[(i*3)+2] == 'o':
+                return True
+        return False
 
     def check_diag(self):
-        if self.board[0] == 'x' and self.board[4] == 'x' and self.board[8] == 'x':
-            return True
-        if self.board[0] == 'o' and self.board[4] == 'o' and self.board[8] == 'o':
-            return True
-        if self.board[2] == 'x' and self.board[4] == 'x' and self.board[6] == 'x':
-            return True
-        if self.board[2] == 'o' and self.board[4] == 'o' and self.board[6] == 'o':
-            return True
-        else:
-            return False
+        for i in range(2):
+            if self.board[i*2] == 'x' and self.board[4] == 'x' and self.board[8-(i*2)] == 'x':
+                return True
+            if self.board[i*2] == 'o' and self.board[4] == 'o' and self.board[8-(i*2)] == 'o':
+                return True
+        return False
 
     def check_column(self):
-        if self.board[0] == 'x' and self.board[3] == 'x' and self.board[6] == 'x':
-            return True
-        if self.board[0] == 'o' and self.board[3] == 'o' and self.board[6] == 'o':
-            return True
-        if self.board[1] == 'x' and self.board[4] == 'x' and self.board[7] == 'x':
-            return True
-        if self.board[1] == 'o' and self.board[4] == 'o' and self.board[7] == 'o':
-            return True
-        if self.board[2] == 'x' and self.board[5] == 'x' and self.board[8] == 'x':
-            return True
-        if self.board[2] == 'o' and self.board[5] == 'o' and self.board[8] == 'o':
-            return True
-        else:
-            return False
+        for i in range(3):
+            if self.board[0+i] == 'x' and self.board[3+i] == 'x' and self.board[6+i] == 'x':
+                return True
+            if self.board[0+i] == 'o' and self.board[3+i] == 'o' and self.board[6+i] == 'o':
+                return True
+        return False
 
     def win(self):
         if self.check_row() or self.check_column() or self.check_diag():
